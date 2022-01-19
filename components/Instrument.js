9328b7ae0237fb70f91b8465d5b5698bbf728789
FDR <delrincon@gmail.com>
2022-01-19 16:56:49 -0300

    dinamic instrumentPage done

--- components/Instrument.js
+++ components/Instrument.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from '../styles/Instrument.module.css';
 
+
 const Instrument = ({ instrument }) => {
     const {Picture, category, description, id, name, price, url} = instrument
     return (
---
+++ pages/instruments/[url].js
@@ -0,0 +1,68 @@
+import Image from "next/image";
+
+import Layout from "../../components/Layout";
+
+import styles from '../../styles/Product.module.css';
+
+
+const Product = ({ instrument }) => {
+    const { Picture, category, description, name, price } = instrument[0];
+
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        alert('Call us... \n 0800-888-123456');
+    }
+
+    return (
+        <>
+            <Layout
+                page={`Instrument ${name}`}
+            >
+                <div className={styles.instrument}>
+                    <Image
+                        priority
+                        layout='responsive'
+                        width={100}
+                        height={180}
+                        src={Picture.url}
+                        alt={`Image product ${name}`}
+                    />
+                    <div className={styles.content}>
+                        <h3>{name}</h3>
+                        <p className={styles.description}>{description}</p>
+                        <p className={styles.price}>$ {price}</p>
+
+                        <p>Do you want it ?</p>
+                        <form 
+                            className={styles.form}
+                            onSubmit={handleSubmit}
+                        >
+                            <input
+                                type="submit"
+                                value="Buy it"
+                                className={styles.button}
+                            />
+                        </form>
+                    </div>
+                </div>
+            </Layout>
+        </>
+    )
+};
+
+export async function getServerSideProps({ query: {url} }) {
+
+    const urlInstrument = `${process.env.API_URL}/instruments?url=${url}`
+    const response = await fetch(urlInstrument);
+    const instrument = await response.json();
+
+    return {
+        props: {
+            instrument
+        }
+    }
+}
+
+export default Product;
